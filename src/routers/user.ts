1457ba0d1367110d1115b23d595941d2e9b22ef2
AsyncFox <dev.asyncfox@gmail.com>
2023-05-29 17:43:05 +0800

    feat(auth): adjust user router logic

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,37 +1,20 @@
 import { router, publicProcedure } from "../trpc";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+// import { insertRefreshToken, insertUser, getUser } from "../db/db";
+import { nanoid } from "nanoid";
 
-// let me explain: because supabase doesn't allows sign up with only username
-// so I have to use a little trick to bypass limitation
-// the email is fake and should be something like 20225300001@shenzhong.tk
-// the real username is stored in `options: { data: { username: input.username } }`
-// the domain shenzhong.tk belongs to me so it's likely to be safe
 export const userRouter = router({
     register: publicProcedure
-        .input(z.object({ username: z.string(), password: z.string().min(8) }))
+        .input(z.object({ username: z.string().max(20), password: z.string().min(8) }))
         .mutation(async ({ ctx, input }) => {
-            let result = await ctx.supabase.auth.signUp({
-                email: input.username + "@shenzhong.tk",
-                password: input.password,
-                options: { data: { username: input.username } },
-            });
-            console.log(result);
-            if (result.error) {
-                throw new TRPCError({ code: "BAD_REQUEST", message: result.error.message });
-            }
-            return result.data;
+            await ctx.auth.register(input.username, input.password);
         }),
     login: publicProcedure
         .input(z.object({ username: z.string(), password: z.string().min(8) }))
         .mutation(async ({ ctx, input }) => {
-            let result = await ctx.supabase.auth.signInWithPassword({
-                email: input.username + "@shenzhong.tk",
-                password: input.password,
-            });
-            if (result.error) {
-                throw new TRPCError({ code: "BAD_REQUEST", message: result.error.message });
-            }
-            return result.data;
+            let user = await ctx.auth.login(input.username, input.password);
+            if (!user) throw new TRPCError({ code: "BAD_REQUEST", message: "Please check your credentials." });
+            return user;
         }),
 });
