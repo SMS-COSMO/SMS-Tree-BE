2f37cb2c4a8d0eadfe827e03dbb8021f7fa21834
AsyncFox <dev.asyncfox@gmail.com>
2023-10-01 21:41:21 +0800

    chore: clear unused code

--- src/db/schema/paper.ts
+++ src/db/schema/paper.ts
@@ -1,18 +1,6 @@
-// import { integer, pgTable, serial, text, timestamp, varchar } from 'drizzle-orm/pg-core'
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 import { nanoid } from 'nanoid'
 
-// export const papers = pgTable('papers', {
-//   id: serial('id').primaryKey(),
-//   title: varchar('title', { length: 64 }).notNull(),
-//   keywords: text('keywords').notNull(),
-//   abstract: text('abstract').notNull(),
-//   authorGroupId: integer('author_group_id').notNull(),
-//   status: integer('status').notNull(),
-//   rate: integer('rate').notNull().default(0),
-//   createdAt: timestamp('created_at').notNull().defaultNow(),
-//   S3FileId: text('s3_file_id').notNull(),
-// })
 export const papers = sqliteTable('papers', {
   id: text('id', { mode: 'text' }).primaryKey().$defaultFn(() => nanoid(12)),
   title: text('title', { mode: 'text' }).notNull(),
