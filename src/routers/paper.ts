eddefe745a95f61899e76182f0b074738e562559
zhangtianli2006 <zhangtianli2006@163.com>
2023-10-23 17:42:22 +0800

    feat(paper): add restriction for routers

--- src/routers/paper.ts
+++ src/routers/paper.ts
@@ -5,11 +5,11 @@ import { publicProcedure, router } from '../trpc'
 export const paperRouter = router({
   create: publicProcedure
     .input(z.object({
-      title: z.string(),
-      keywords: z.array(z.string()),
+      title: z.string().min(1, { message: '请输入论文标题' }).max(256, { message: '论文标题长度不应超过 256' }),
+      keywords: z.string().array(),
       abstract: z.string(),
-      authorGroupId: z.string(),
-      S3FileId: z.string(),
+      authorGroupId: z.string().nonempty('无效作者组'),
+      S3FileId: z.string().nonempty('请上传文件'),
     }))
     .mutation(async ({ ctx, input }) => {
       const res = await ctx.paperController.create(input)
@@ -20,7 +20,7 @@ export const paperRouter = router({
     }),
 
   content: publicProcedure
-    .input(z.object({ id: z.string() }))
+    .input(z.object({ id: z.string().nonempty('论文id不存在') }))
     .query(async ({ ctx, input }) => {
       const res = await ctx.paperController.getContent(input.id)
       if (!res.res || !res.success)
@@ -39,7 +39,7 @@ export const paperRouter = router({
     }),
 
   file: publicProcedure
-    .input(z.object({ id: z.string() }))
+    .input(z.object({ id: z.string().nonempty('论文id不存在') }))
     .query(async ({ ctx, input }) => {
       const res = await ctx.paperController.getFile(input.id)
       if (!res.res || !res.success)
