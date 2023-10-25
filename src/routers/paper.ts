2b9cc4f31d16f0807e1099b91c5e19a72e622d1e
zhangtianli2006 <zhangtianli2006@163.com>
2023-10-25 22:16:19 +0800

    feat(paper): set keyword restriction to 8

--- src/routers/paper.ts
+++ src/routers/paper.ts
@@ -6,7 +6,7 @@ export const paperRouter = router({
   create: protectedProcedure
     .input(z.object({
       title: z.string().min(1, { message: '请输入论文标题' }).max(256, { message: '论文标题长度不应超过 256' }),
-      keywords: z.array(z.string().max(20, { message: '关键词最长为20个字符' })).max(20, { message: '最多100个关键词' }),
+      keywords: z.array(z.string().max(8, { message: '关键词最长为8个字符' })).max(8, { message: '最多8个关键词' }),
       abstract: z.string(),
       authorGroupId: z.string().nonempty('无效作者组'),
       S3FileId: z.string().nonempty('请上传文件'),
