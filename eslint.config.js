a53718b36952943ebb25092f7d8154961dad982b
AsyncFox <dev.asyncfox@gmail.com>
2023-10-29 17:04:37 +0800

    style: turn off linebreak-style rule

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,6 @@ import antfu from '@antfu/eslint-config'
 export default antfu({}, {
   rules: {
     'no-console': 'off',
-    'linebreak-style': ['warn', 'windows'],
+    'linebreak-style': 'off',
   },
 })
