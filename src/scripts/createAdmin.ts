f3be2b7c33fe29ca3ffd489d7f0fea608e5a1512
AsyncFox <59872475+AsyncFox@users.noreply.github.com>
2023-11-03 23:30:33 +0800

    chore: update default admin username & password
    Co-authored-by: ZTL-UwU<zhangtianli2006@163.com>

--- src/scripts/createAdmin.ts
+++ src/scripts/createAdmin.ts
@@ -1,8 +1,8 @@
 import { UserController } from '../controllers/user';
 
 const userController = new UserController();
-await userController.register({ username: 'admin1', id: 'admin1', password: 'admin123', role: 'admin' });
-console.log('Created default admin user. \nUserID: `admin1` \nUsername: `admin1` \nPassword: `admin123`');
-const admin = await userController.login('admin1', 'admin123');
+await userController.register({ username: 'admin', id: 'admin', password: '12345678', role: 'admin' });
+console.log('Created default admin user. \nUserID: `admin` \nUsername: `admin` \nPassword: `12345678`');
+const admin = await userController.login('admin', '12345678');
 console.log('Admin AccessToken:');
 console.log(admin?.accessToken);
