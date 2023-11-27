50a3cc72dc788b6713344d483f3badb08989f36a
zhangtianli2006 <zhangtianli2006@163.com>
2023-11-27 22:25:12 +0800

    refector(script): use random password for createAdmin

--- src/scripts/createAdmin.ts
+++ src/scripts/createAdmin.ts
@@ -1,8 +1,10 @@
+import { nanoid } from 'nanoid';
 import { UserController } from '../controllers/user';
 
 const userController = new UserController();
-await userController.register({ username: 'admin', id: 'admin', password: '12345678', role: 'admin' });
-console.log('Created default admin user. \nUserID: `admin` \nUsername: `admin` \nPassword: `12345678`');
-const admin = await userController.login('admin', '12345678');
+const password = nanoid(10);
+await userController.register({ username: 'admin', id: 'admin', password: password, role: 'admin' });
+console.log(`Created default admin user. \nUserID: 'admin' \nUsername: 'admin' \nPassword: '${password}'`);
+const admin = await userController.login('admin', password);
 console.log('Admin AccessToken:');
 console.log(admin?.accessToken);
